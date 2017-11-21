377b24b5acfa5d8b46348e08fa8b651b39fc0aa2
gawain <yuuki1993server@gmail.com>
2017-11-21 21:44:55 +0900

    add

--- GCD/Euclidean_Algorithm.py
+++ GCD/Euclidean_Algorithm.py
@@ -3,7 +3,15 @@ if __name__ == '__main__':
     N1 = int(input())
     N2 = int(input())
 
-    while (N1 == 0 or N2 == 0):
-        if(N1 < N2):
-
-
+    while True:
+        if (N1 == 0 or N2 == 0):
+            if(N1 > N2):
+                print(N1)
+                break
+            else:
+                print(N2)
+                break
+        elif(N1 < N2):
+            N2 = N2 % N1
+        else:
+            N1 = N1 % N2
